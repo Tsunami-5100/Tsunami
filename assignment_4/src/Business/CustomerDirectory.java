4ecadc0cd3697e6195a643efe71565b0369c085a
Edmond-John <zhang.shijie@northeastern.edu>
2020-11-01 12:19:10 +0800

    ass4

---
+++ assignment_4/src/Business/CustomerDirectory.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author edmond
+ */
+public class CustomerDirectory {
+    
+    private List<Customer> customerDirectory;
+       
+    public CustomerDirectory(){
+        customerDirectory =  new ArrayList<Customer>();
+    }  
+
+    public List<Customer> getCustomerDirectory() {
+        return customerDirectory;
+    }
+
+    public void setCustomerDirectory(List<Customer> customerDirectory) {
+        this.customerDirectory = customerDirectory;
+    }
+    public Customer addCustomer(){
+        Customer a = new Customer();
+        customerDirectory.add(a);
+        return a;
+    }
+    
+    public void removeCustomer(Customer a){
+        customerDirectory.remove(a);
+    }
+}
